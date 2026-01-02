8ebc4af6e71b72d372e011de06586a8d247d841e
mattfurness <matt.furness@gmail.com>
2026-01-02 11:45:53 +1000

    nix release-25.11

--- config/apps.nix
+++ config/apps.nix
@@ -8,7 +8,7 @@
       apps = pkgs.buildEnv {
         name = "home-manager-applications";
         paths = config.home.packages;
-        pathsToLink = "/Applications";
+        pathsToLink = [ "/Applications" ];
       };
     in "${apps}/Applications";
   };
